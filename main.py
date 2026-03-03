1a9952d49276611dfdfe38bd4b9087514e74c6a9
Rish-44-tech <jalanrishit@gmail.com>
2026-03-03 21:52:59 +0530

    Added main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+def error():
+        return
+
+def checkType():
+        return
+
+def rType():
+        return
+
+def sTypr():
+        return
+
+def bType():
+        return
+
+def sType():
+        return
+
+def uType():
+        return
+
+def jType():
+        return
+
+def iType():
+        return
+
+#main
+
+"""Write main here"""
