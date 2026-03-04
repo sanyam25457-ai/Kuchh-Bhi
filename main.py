c99e943e0f3a1cfb2fd7e4fa4d05df4905231345
Rish-44-tech <jalanrishit@gmail.com>
2026-03-04 18:04:38 +0530

    .

--- main.py
+++ main.py
@@ -213,7 +213,7 @@ def main():
                 try:
                         isLabel = 0 if(":" not in instruction) else 1
                         binInstruction = convert(instruction, isLabel, i)
-                        
+                        pc+=1
                         #Write binary string onto file
                 
                 except ZeroDivisionError:
