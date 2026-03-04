9fbdb9aad80837813fc1d967f68154a543f1657f
Rish-44-tech <jalanrishit@gmail.com>
2026-03-04 16:16:51 +0530

    modifed fns

--- main.py
+++ main.py
@@ -15,6 +15,53 @@
 
 #7. call corr() to remove commas
 
+instructions={
+    "add":"R",
+    "sub":"R",
+    "slt":"R",
+    "sltu":"R",
+    "xor":"R",
+    "sll":"R",
+    "srl":"R",
+    "or":"R",
+    "and":"R",
+    "lw":"I",
+    "addi":"I",
+    "sltiu":"I",
+    "jalr":"I",
+    "sw":"S",
+    "beq":"B",
+    "bne":"B",
+    "bge":"B",
+    "bgeu":"B",
+    "blt":"B",
+    "bltu":"B",
+    "auipc":"U",
+    "lui":"U",
+    "jal":"J"
+}
+
+registers = {
+    "zero": 0, "ra": 1, "sp": 2, "gp": 3, "tp": 4,
+    "t0": 5, "t1": 6, "t2": 7,
+    "s0": 8, "fp": 8, "s1": 9,
+    "a0": 10, "a1": 11, "a2": 12, "a3": 13,
+    "a4": 14, "a5": 15, "a6": 16, "a7": 17,
+    "s2": 18, "s3": 19, "s4": 20, "s5": 21,
+    "s6": 22, "s7": 23, "s8": 24, "s9": 25,
+    "s10": 26, "s11": 27,
+    "t3": 28, "t4": 29, "t5": 30, "t6": 31,
+    "x0": 0, "x1": 1, "x2": 2, "x3": 3, "x4": 4,
+    "x5": 5, "x6": 6, "x7": 7,
+    "x8": 8, "x9": 9, "x10": 10, "x11": 11,
+    "x12": 12, "x13": 13, "x14": 14, "x15": 15,
+    "x16": 16, "x17": 17, "x18": 18, "x19": 19,
+    "x20": 20, "x21": 21, "x22": 22, "x23": 23,
+    "x24": 24, "x25": 25, "x26": 26, "x27": 27,
+    "x28": 28, "x29": 29, "x30": 30, "x31": 31
+}
+
+
 def convert(inst:str) -> str:
         instType = checkType(inst)
 
@@ -34,8 +81,13 @@ def convert(inst:str) -> str:
                 case _:
                         raise ZeroDivisionError
 
-def checkType(inst:str):
-        pass
+def checkType(inst:str)->str:
+        inst=inst.split()
+        temp=inst[0]
+        if temp in instructions:
+                return instructions.get(temp)
+        
+        return "Not Found"       
 
 def signExt(binString:str, funcType:str) -> str:
         """
@@ -73,7 +125,57 @@ def rType(inst:str) -> str:
         pass
 
 def iType(inst:str) -> str:
-        pass
+        inst=inst.split()
+        binInst=""
+        match(inst[0]):
+                case "addi":
+                        opcode="0010011"
+                        funct3="000"
+                case "lw":
+                        opcode="0000011"
+                        funct3="010"
+
+                case "sltiu":
+                        opcode="0010011"
+                        funct3="011"
+                case "jalr":
+                        opcode="1100111"
+                        funct3="000"
+                case _:
+                        raise ZeroDivisionError
+                
+        rd=corr(inst[1])
+        if rd not in registers:
+                raise ZeroDivisionError
+        else:
+                rd=format(rd,"05b")
+
+        if opcode=="0010011" or opcode=="1100111":
+                if(len(inst)!=4):
+                        raise ZeroDivisionError
+                else:
+                        rs=corr(inst[2])
+                        imm=inst[3]
+        else:
+                temp=inst[2].split("(")
+                if len(temp)!=2:
+                        raise ZeroDivisionError
+                temp[1]=temp[1].rstrip(")")
+                imm=temp[0]
+                rs=temp[1]
+
+        if rs not in registers:
+                raise ZeroDivisionError
+        else:
+                rs=format(rs,"05b")
+        
+        if not imm.lstrip("-").isdigit() or int(imm)>2047 or int(imm)<-2048:
+                raise ZeroDivisionError
+        else:
+                imm=signExt(imm,"I")
+                imm=imm[-12:]   #since signExt returns 32 bit imm and I need 12 bit imm here
+        
+        binInst=imm+funct3+rs+rd+opcode
 
 def sTypr(inst:str) -> str:
         pass
