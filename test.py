3e97b115a9aa67122918d755b0839adbf4e81900
sanyam25457-ai <sanyam25457@iiitd.ac.in>
2026-03-05 12:00:00 +0530

    .

--- test.py
+++ test.py
@@ -1,1 +1,31 @@
-print(bin(-6))
+def signExt(binString:str, funcType:str) -> str:
+        """
+        Isolate the immediate bits put them in CORRECT ORDER and then pass it as an argument as string.
+        
+        try-catch for ValueError and ZeroDivisonError when using signExt()
+        
+        Only pass the IMMEDIATE BINARY not THE ENTIRE INSTRUCTION.
+        """
+        result_imm = ""
+        
+        
+        if(funcType.upper() == "R"):
+                raise ValueError
+        
+        elif(funcType.upper() == "I" or funcType.upper() == "S"):
+                result_imm = "0b" + (20*binString[2]) + binString[2:]
+
+        
+        elif(funcType.upper() == "B"):
+                result_imm = "0b" + (19*binString[2]) + binString[2:] + "0"
+
+        elif(funcType.upper() == "U"):
+                result_imm = binString + (12*"0")
+        
+        elif(funcType.upper() == "J"):
+                result_imm = "0b" + (12*binString[2]) + binString[3:] + "0"
+
+        else:
+                raise ZeroDivisionError
+
+        return result_imm
