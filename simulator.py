45ed95d7c2f149ac0ddcf90a70dc41415f7afe4f
sanyam25457-ai <sanyam25457@iiitd.ac.in>
2026-03-26 16:03:46 +0530

    created framework and added comments

--- simulator.py
+++ simulator.py
@@ -1,3 +1,28 @@
+#1. For any functions requiring immediate exted the immediate by passing the required parameters in the signExt()
+#   function.
+
+#2. As done in assembler extraction of opcode will give the type of the function and the task.
+
+#3. For functions with funct7 and funct3 differentiating between instructions, create an internal dictionary within
+#   the function type.
+
+#4. Check for errors and raise ZeroDivisionError, whenever an error is encountered.
+
+
+opcodes = {
+        "0110011" : "R",
+        "0000011" : "I",
+        "0010011" : "I",
+        "1100111" : "I",
+        "0100011" : "S",
+        "1100011" : "B",
+        "0110111" : "U",
+        "0010111" : "U",
+        "1101111" : "J"
+}
+
+
+
 def signExt(binString:str, funcType:str) -> str:
         """
         #Isolate the immediate bits put them in CORRECT ORDER and then pass it as an argument as string.
@@ -30,9 +55,27 @@ def signExt(binString:str, funcType:str) -> str:
 
         return result_imm
 
+def Rtype():
+        pass
+
+def Itype():
+        pass
+
+def Stype():
+        pass
+
+def Btype():
+        pass
+
+def Utype():
+        pass
+
+def Jtype():
+        pass
+
 def main():
         return
 
-
+# remove pass after function is created
 if __name__ == "__main__":
         main()
